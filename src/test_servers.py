5d466329af91b3e0ed2804704ae1e6a9aff8c97b
Norton Pengra <npengra317@gmail.com>
2016-03-07 21:26:24 -0800

    Added docstrings to tests

--- src/test_servers.py
+++ src/test_servers.py
@@ -4,24 +4,28 @@ import client
 
 
 def test_echo_is_identical():
+    """Test if echo is same text that was sent"""
     message = "500 worth of waffles"
     assert client.client(message) == message
 
 
 def test_buffer_minus_one():
+    """Test if echo is same text that was sent if len(message) is buffer_length - 1"""
     message = "a" * 1023
     assert client.client(message) == message
 
 
 def test_several_buffers():
+    """Test if echo is same text that was sent if len(message) is a multiple of buffer_length"""
     message = ("a" * 1024) + ("b" * 1024)
     assert client.client(message) == message
 
 
 def test_non_ascii():
+    """Test if echo is same text that was sent if message contains non-ascii character"""
     message = "£500 worth of waffles"
 
-    # hasattr determines the python version, if the string has the ability
+    # hasattr() determines the python version, if the string has the ability
     # to be decoded, then we are running python2 and requires the string to be
     # decoded and encoded.
 
