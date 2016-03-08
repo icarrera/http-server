6ee27cc056494281e21209aa1dd1f8634d4ae110
icarrera <irismcarrera@gmail.com>
2016-03-07 18:03:15 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='echo server',
+    description='A Python application stufff here ',
+    version=0.1,
+    author='Norton Pengra and Iris Carrera',
+    license='MIT',
+    py_modules=['client', 'server'],
+    package_dir={'': 'src'},
+    extras_require={'test': ['pytest', 'pytest-xdist', 'tox']},
+)
