43e49edad8a2e35a86b7474675470d71970ffd62
Norton Pengra <npengra317@gmail.com>
2016-03-08 15:46:26 -0800

    Created response codes and added logging

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 # -*-coding:utf-8-*-
 """Handle server operations of reading incoming streams and echoing them"""
 import socket
-
+import email.utils
 
 buffer_length = 4
 PORT = 5000
@@ -27,7 +27,7 @@ def server_read(connection):
     """Read and parse message from client"""
 
     # XXX 3/6/16
-    # What we should've done was exclude the if statement and call server_echo()
+    # What we should've done was exclude the if statement and call server_response()
     # REGARDLESS of whether we've recieved the whole message or not, because
     # We don't care, we just want to echo immediately.
 
@@ -40,9 +40,20 @@ def server_read(connection):
     return (string.decode('utf-8'), connection)
 
 
-def server_echo(string, connection):
+def server_response(string, connection):
     """Send back specified string to specified connection"""
     connection.send(string.encode('utf-8'))
+
+
+def response_ok(connection):
+    """Send back an HTTP 200 OK status message"""
+    server_response("HTTP/1.1 200 OK<CRLF>\n", connection)
+    server_response("<CRLF>\n", connection)
+
+
+def response_error(connection):
+    """Send back an HTTP 500 error message"""
+    server_response("HTTP/1.1 500 Internal Server Error", connection)
 
 
 def server():
@@ -52,7 +63,8 @@ def server():
         while True:
             connection, address = server_listen(socket)
             result, connection = server_read(connection)
-            server_echo(result, connection)
+            print("log:", result)
+            server_response(result, connection)
     except KeyboardInterrupt:
         print("Closing the server!")
     finally:
